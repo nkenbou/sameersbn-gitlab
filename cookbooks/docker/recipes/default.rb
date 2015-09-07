618b06ff2adb822a413210d1ab63d1e614bb9d09
nkenbou <nkenbou@gmail.com>
2015-09-08 06:28:33 +0900

    docker のレシピを調整中

--- cookbooks/docker/recipes/default.rb
+++ cookbooks/docker/recipes/default.rb
@@ -8,9 +8,9 @@
 #
 bash "install docker" do
   code <<-EOC
-    curl -L https://get.docker.io/ubuntu/ | sh
+    curl -sSL https://get.docker.com/ | sh
   EOC
-  creates "/usr/"
+  not_if 'which docker'
 end
 
 bash "install docker-compose" do
@@ -18,19 +18,18 @@ bash "install docker-compose" do
     curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
   EOC
-  creates "/usr/local/bin/docker-compose"
-  notifies :restart, "service[docker]"
+  not_if 'which docker-compose'
 end
 
 bash "edit /etc/default/docker" do
   code <<-EOC
     echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' >> /etc/default/docker
   EOC
-  notifies :reload, 'service[docker]'
+  notifies :restart, 'service[docker]'
 end
 
 service "docker" do
-  # provider Chef::Provider::Service::Upstart
+  provider Chef::Provider::Service::Upstart
   action [ :enable, :start ]
   supports :restart => true, :reload => true
 end
