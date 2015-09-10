cde30f1da46eb701c2e229df6d8019edae68efa0
nkenbou <nkenbou@gmail.com>
2015-09-10 19:14:42 +0900

    go ソースをデファクトなフォーマットにした

--- dproxy/linked/src/linked/main.go
+++ dproxy/linked/src/linked/main.go
@@ -1,82 +1,82 @@
 package main
 
 import (
-  "fmt"
-  "github.com/fsouza/go-dockerclient"
-  "github.com/garyburd/redigo/redis"
-  "os"
-  "strings"
-  "time"
+	"fmt"
+	"github.com/fsouza/go-dockerclient"
+	"github.com/garyburd/redigo/redis"
+	"os"
+	"strings"
+	"time"
 )
 
 var (
-  docker_endpoint string
-  redis_endpoint  string
+	docker_endpoint string
+	redis_endpoint  string
 )
 
 func httpPort(port int64) bool {
-  return port == 80 ||
-    port == 3000 ||
-    port == 5000 ||
-    port == 8000 ||
-    port == 8080 ||
-    port == 10080
+	return port == 80 ||
+		port == 3000 ||
+		port == 5000 ||
+		port == 8000 ||
+		port == 8080 ||
+		port == 10080
 }
 
 func getDests() map[string]string {
-  dests := make(map[string]string)
-  client, _ := docker.NewClient(docker_endpoint)
-  opts := docker.ListContainersOptions{}
-  containers, _ := client.ListContainers(opts)
-  for _, container := range containers {
-    for _, port := range container.Ports {
-      if port.IP == "0.0.0.0" &&
-         httpPort(port.PrivatePort) &&
-         port.Type == "tcp" {
-           inspect, _ := client.InspectContainer(container.ID)
-           containerName := strings.TrimLeft(inspect.Name, "/")
-           dest := fmt.Sprintf("%s:%d",
-             inspect.NetworkSettings.IPAddress,
-             port.PrivatePort)
-           dests[containerName] = dest
-      }
-    }
-  }
-  return dests
+	dests := make(map[string]string)
+	client, _ := docker.NewClient(docker_endpoint)
+	opts := docker.ListContainersOptions{}
+	containers, _ := client.ListContainers(opts)
+	for _, container := range containers {
+		for _, port := range container.Ports {
+			if port.IP == "0.0.0.0" &&
+				httpPort(port.PrivatePort) &&
+				port.Type == "tcp" {
+				inspect, _ := client.InspectContainer(container.ID)
+				containerName := strings.TrimLeft(inspect.Name, "/")
+				dest := fmt.Sprintf("%s:%d",
+					inspect.NetworkSettings.IPAddress,
+					port.PrivatePort)
+				dests[containerName] = dest
+			}
+		}
+	}
+	return dests
 }
 
 func setToRedis(dests map[string]string) {
-  client, err := redis.Dial("tcp", redis_endpoint)
-  if err != nil {
-    fmt.Println("fail to connect redis server: ", err)
-    return
-  }
-  defer client.Close()
-  for name, dest := range dests {
-    client.Do("SET", name, dest)
-  }
+	client, err := redis.Dial("tcp", redis_endpoint)
+	if err != nil {
+		fmt.Println("fail to connect redis server: ", err)
+		return
+	}
+	defer client.Close()
+	for name, dest := range dests {
+		client.Do("SET", name, dest)
+	}
 }
 
 func main() {
-  docker_endpoint = "unix:///var/run/docker.sock"
-  redis_endpoint = "127.0.0.1:6379"
-  if os.Getenv("DOCKER_HOST") != "" {
-    docker_endpoint = os.Getenv("DOCKER_HOST")
-  }
-  if os.Getenv("REDIS_ENDPOINT") != "" {
-    redis_endpoint = os.Getenv("REDIS_ENDPOINT")
-  } else if os.Getenv("REDIS_PORT_6379_TCP_ADDR") != "" &&
-    os.Getenv("REDIS_PORT_6379_TCP_PORT") != "" {
-    redis_endpoint = fmt.Sprintf("%s:%s",
-      os.Getenv("REDIS_PORT_6379_TCP_ADDR"),
-      os.Getenv("REDIS_PORT_6379_TCP_PORT"))
-  }
-  fmt.Println("docker_endpoint:", docker_endpoint)
-  fmt.Println("redis_endpoint:", redis_endpoint)
-  
-  for {
-    dests := getDests()
-    setToRedis(dests)
-    time.Sleep(10 * time.Second)
-  }
+	docker_endpoint = "unix:///var/run/docker.sock"
+	redis_endpoint = "127.0.0.1:6379"
+	if os.Getenv("DOCKER_HOST") != "" {
+		docker_endpoint = os.Getenv("DOCKER_HOST")
+	}
+	if os.Getenv("REDIS_ENDPOINT") != "" {
+		redis_endpoint = os.Getenv("REDIS_ENDPOINT")
+	} else if os.Getenv("REDIS_PORT_6379_TCP_ADDR") != "" &&
+		os.Getenv("REDIS_PORT_6379_TCP_PORT") != "" {
+		redis_endpoint = fmt.Sprintf("%s:%s",
+			os.Getenv("REDIS_PORT_6379_TCP_ADDR"),
+			os.Getenv("REDIS_PORT_6379_TCP_PORT"))
+	}
+	fmt.Println("docker_endpoint:", docker_endpoint)
+	fmt.Println("redis_endpoint:", redis_endpoint)
+
+	for {
+		dests := getDests()
+		setToRedis(dests)
+		time.Sleep(10 * time.Second)
+	}
 }
