af90eca12d6d3b1d1d19cc6d9e1a72f28bb0dcc9
nkenbou <nkenbou@gmail.com>
2015-12-08 10:33:03 +0900

    docker-compose のアップデート

--- cookbooks/docker/recipes/default.rb
+++ cookbooks/docker/recipes/default.rb
@@ -15,7 +15,7 @@ end
 
 bash "install docker-compose" do
   code <<-EOC
-    curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
   EOC
   not_if 'which docker-compose'
